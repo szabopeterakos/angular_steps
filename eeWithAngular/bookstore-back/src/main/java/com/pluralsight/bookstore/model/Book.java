918bc0b18fbec3b65f6380712273fbd5f8012c07
Péter Szabó B <peter.b.szabo@ericsson.com>
2018-11-21 14:44:58 +0100

    none

---
+++ eeWithAngular/bookstore-back/src/main/java/com/pluralsight/bookstore/model/Book.java
@@ -0,0 +1,119 @@
+package com.pluralsight.bookstore.model;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue
+    private Long Id;
+    @Column(length = 200)
+    private String title;
+    @Column(length = 2000)
+    private String description;
+    @Column(name = "unit_cost")
+    private Float unitCost;
+    private String isbn;
+    @Column(name = "publication_date")
+    @Temporal(TemporalType.DATE)
+    private Date publicationDate;
+    @Column(name = "number_of_pages")
+    private Integer nbOfPages;
+    @Column(name = "image_url")
+    private String imageURL;
+    private Language language;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Float getUnitCost() {
+        return unitCost;
+    }
+
+    public void setUnitCost(Float unitCost) {
+        this.unitCost = unitCost;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Date getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(Date publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public Integer getNbOfPages() {
+        return nbOfPages;
+    }
+
+    public void setNbOfPages(Integer nbOfPages) {
+        this.nbOfPages = nbOfPages;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public Language getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(Language language) {
+        this.language = language;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "Id=" + Id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", unitCost=" + unitCost +
+                ", isbn='" + isbn + '\'' +
+                ", publicationDate=" + publicationDate +
+                ", nbOfPages=" + nbOfPages +
+                ", imageURL='" + imageURL + '\'' +
+                ", language=" + language +
+                '}';
+    }
+}
---
+++ eeWithAngular/bookstore-back/src/main/java/com/pluralsight/bookstore/model/Language.java
@@ -0,0 +1,7 @@
+package com.pluralsight.bookstore.model;
+
+public enum Language {
+
+    ENGLISH,FRENCH,ITALIAN,DEUTCH,HUGARIAN
+
+}
