cbe8dc473e88adc6cd7b11802c5130e3db6062d1
szabopeterakos <szabopeterakos@gmail.com>
2019-07-23 22:39:42 +0200

    [communication] Add buttons to interactions

--- communication/src/app/app.module.ts
+++ communication/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { GreenComponent } from './green/green.component';
@@ -8,6 +8,11 @@ import { YellowComponent } from './yellow/yellow.component';
 import { OrangeComponent } from './orange/orange.component';
 import { WhiteComponent } from './white/white.component';
 import { HttpClientModule } from '@angular/common/http';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule, MatIconModule } from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -21,8 +26,16 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule, 
+    ReactiveFormsModule,
+    CommonModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCheckboxModule,
   ],
   providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule {
--- communication/src/app/orange/orange.component.ts
+++ communication/src/app/orange/orange.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-orange',
