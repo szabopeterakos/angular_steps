a58393cd4fd394a4875d3592b19d97f0a185cb31
szabopeterakos <szabopeterakos@gmail.com>
2019-09-22 17:37:13 +0200

    [something]

--- communication/src/app/orange/orange.component.ts
+++ communication/src/app/orange/orange.component.ts
@@ -9,9 +9,27 @@ import {MatIconModule} from '@angular/material/icon';
 })
 export class OrangeComponent implements OnInit {
 
+  number: number = 0;
+  show: boolean =  false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  clickHandler(){
+    this.number += 1;
+    console.log(`click activated`)
+  }
+
+  hoverHandler(){
+    this.number = 0;
+    this.show = true;
+    console.log(`hover activated`)
+  }
+
+  hoverOffHandler(){
+    this.show = false;
+    console.log(`hover Off activated`)
+  }
 }
