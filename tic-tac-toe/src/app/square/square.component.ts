8a82537c4f2605c2e54e93919e861cdb71bf18a6
szabopeterakos <szabopeterakos@gmail.com>
2019-10-05 09:35:07 +0200

    add setup for prettier and editorconfig

---
+++ tic-tac-toe/src/app/square/square.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  template: `
+    <p>
+      square works!
+    </p>
+  `,
+  styleUrls: ['./square.component.sass']
+})
+export class SquareComponent {}
