9b8c70ad8da5e2cf7f0e5d36f5cf1d056cee1b37
szabopeterakos <szabopeterakos@gmail.com>
2019-10-09 21:00:54 +0200

    some animation

--- socialMediaButtons/src/app/app.component.ts
+++ socialMediaButtons/src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { faAngry, faHorse,faDiceOne,faFighterJet,faHatWizard } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  faCoffee = faCoffee;
+  faAngry = faAngry;
+  faHorse = faHorse;
+  faDiceOne = faDiceOne;
+  faFighterJet =faFighterJet;
+  faHatWizard=faHatWizard;
   title = 'socialMediaButtons';
 }
--- socialMediaButtons/src/app/app.module.ts
+++ socialMediaButtons/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
