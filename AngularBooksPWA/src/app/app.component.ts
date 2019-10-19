74da16f60693ba16e062cf27c688393f783c4d85
peterszabo <szabopeterakos@gmail.com>
2019-10-19 17:39:22 +0200

    [AngularBooksPWA] init

---
+++ AngularBooksPWA/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'AngularBooksPWA';
+  tutorialURL = 'https://developer.okta.com/blog/2019/01/30/first-angular-pwa';
+}
